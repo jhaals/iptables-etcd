45f182275e416b865a4d2132292fb8973276c1f2
Johan Haals <johan.haals@gmail.com>
2014-09-15 12:51:44 +0200

    watch reload entry in etcd.

--- main.go
+++ main.go
@@ -2,30 +2,29 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"time"
 )
 
-type Rule struct {
+type Entry struct {
 	Key   string
 	Value string
 }
-type Iptables struct {
-	Rules    []Rule `json:"nodes"`
-	Modified int    `json:"ModifiedIndex"`
-	Created  int    `json:"CreatedIndex"`
+type Result struct {
+	Entries  []Entry `json:"nodes"`
+	Modified int     `json:"ModifiedIndex"`
+	Created  int     `json:"CreatedIndex"`
 }
 type Response struct {
-	Iptables Iptables `json:"node"`
+	Result Result `json:"node"`
 }
 
-func getRules(url string) (Iptables, error) {
+func etcdGet(url string) (Result, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Println(err)
@@ -34,13 +33,13 @@ func getRules(url string) (Iptables, error) {
 	body, err := ioutil.ReadAll(resp.Body)
 	var response Response
 	if err != nil {
-		return response.Iptables, err
+		return response.Result, err
 	}
 	json.Unmarshal(body, &response)
-	return response.Iptables, nil
+	return response.Result, nil
 }
 
-func writeRules(rules []Rule, file string) error {
+func writeRules(rules []Entry, file string) error {
 	f, err := os.Create(file)
 	if err != nil {
 		return err
@@ -54,6 +53,39 @@ func writeRules(rules []Rule, file string) error {
 	return nil
 }
 
+func UpdateRules(host string, file string) error {
+	r, err := etcdGet("http://" + host + "/v2/keys/iptables/rules?sorted=true")
+	if err != nil {
+		// No response from etcd
+		return err
+	}
+
+	// We shouldn't write less than 3 rules.
+	if len(r.Entries) < 3 {
+		return errors.New("Dude! There's not enough rules in etcd...")
+	}
+
+	// Write rules from etcd to file
+	err = writeRules(r.Entries, file)
+	if err != nil {
+		return err
+	}
+	log.Println(file, "updated")
+	return nil
+}
+
+func EtcdWatch(host string) {
+	log.Println("Watching etcd for changes")
+	for {
+		time.Sleep(time.Second)
+		_, err := etcdGet("http://" + host + "/v2/keys/iptables/reload?wait=true")
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		return
+	}
+}
 func main() {
 	host := "172.17.42.1:4001"
 	dst := os.Getenv("DST")
@@ -61,44 +93,11 @@ func main() {
 		log.Println("Destination path 'DST' must be set")
 		os.Exit(1)
 	}
-	id := os.Getenv("ID")
-	if dst == "" {
-		log.Println("ID must be set")
-		os.Exit(1)
-	}
-	rulesFile := filepath.Join(dst, "iptables.rules")
-	log.Println("Starting... Going to watch etcd for changes")
 	for {
-		// Wait 5 sec just in case something goes bananas.
-		time.Sleep(time.Second * 5)
-		url := func() string {
-			if _, err := os.Stat(rulesFile); err != nil {
-				return "http://" + etcd + "/v2/keys/iptables?sorted=true"
-			}
-			return "http://" + etcd + "/v2/keys/iptables?sorted=true&wait=true"
-		}
-		r, err := getRules(url())
-		if err != nil {
-			// No response from etcd
-			log.Println(err)
-			continue
-		}
-
-		// We shouldn't write less than 3 rules.
-		if len(r.Rules) < 3 {
-			log.Println("Dude! There's not enough rules in etcd...")
-			continue
-		}
-
-		// Write rules from etcd to file
-		err = writeRules(r.Rules, rulesFile)
+		EtcdWatch(host)
+		err := UpdateRules(host, filepath.Join(dst, "iptables.rules"))
 		if err != nil {
 			log.Println(err)
-			continue
 		}
-		log.Println(rulesFile, "updated")
-		// Fix this..
-		exec.Command(fmt.Sprintf("curl -L -XPUT http://%s/v2/keys/iptables-reload/%s -d value=reload", etcd, id)).Output()
-		continue
 	}
 }
